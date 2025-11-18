951d757679ba310a3ff90b19ca0f5a828be82b82
Sachin <dpoolkr@gmail.com>
2025-11-19 01:20:41 +0530

    outlet added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
 import { UIProvider } from "./UIContext";
-import { HashRouter as Router, Routes, Route } from "react-router-dom"; // <-- HashRouter
-import BackgroundMain from "./components/background/BackgroundMain.jsx";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Nav from "./components/nav.jsx";
 import Component from "./components/Component.jsx";
@@ -10,6 +9,14 @@ import LiquidChrome from "./components/LiquidChrome.jsx";
 import Test from "./components/test.jsx";
 import Footer from "./components/footer.jsx"; 
 import Hero from "./components/Hero.jsx";
+import Background1 from "./components/background/Background1.jsx";
+import Background2 from "./components/background/Background2.jsx";
+import Background3 from "./components/background/Background3.jsx";
+import Card1 from "./components/card/Card1.jsx";
+import Card2 from "./components/card/Card2.jsx";
+import Gojo from "./components/card/gojo.jsx";
+import Form1 from "./components/form/Form1.jsx";
+import Form2 from "./components/form/Form2.jsx";
 
 function Home() {
   return (
@@ -29,7 +36,16 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/components" element={<Component />} />
+          <Route path="/components" element={<Component />}>
+            <Route path="background1" element={<Background1 />} />
+            <Route path="background2" element={<Background2 />} />
+            <Route path="background3" element={<Background3 />} />
+            <Route path="card1" element={<Card1 />} />
+            <Route path="card2" element={<Card2 />} />
+            <Route path="gojo" element={<Gojo />} />
+            <Route path="form1" element={<Form1 />} />
+            <Route path="form2" element={<Form2 />} />
+          </Route>
         </Routes>
       </Router>
     </UIProvider>
--- frontend/src/components/component.jsx
+++ frontend/src/components/component.jsx
@@ -1,13 +1,11 @@
 import React, { useContext } from "react";
-import { UIContext } from "../UIContext"; // import context
-import BackgroundMain from "./background/BackgroundMain.jsx";
-import FormMain from "./form/FormMain.jsx";
-import CardMain from "./card/CardMain.jsx";
+import { UIContext } from "../UIContext";
+import { Link, Outlet } from "react-router-dom";
 import "./component.css";
 import MainSection from "./MainSection";
 
 function Component() {
-  const { openList, setOpenList, setSelectedComponent } = useContext(UIContext);
+  const { openList, setOpenList } = useContext(UIContext);
 
   function handleClick(listName) {
     setOpenList(openList === listName ? null : listName);
@@ -17,41 +15,57 @@ function Component() {
     <div className="content">
       <aside className="sidebar">
         <h1 className="shiny-text">Index</h1>
+
         <ul>
-          <h2 className="shiny-text" onClick={() => handleClick("Background")}>Background</h2>
-          {openList === "Background" && (
-            <ul>
-              <li onClick={() => setSelectedComponent("Background1")}>Background1</li>
-              <li onClick={() => setSelectedComponent("Background2")}>Background2</li>
-              <li onClick={() => setSelectedComponent("Background3")}>Background3</li>
-            </ul>
-          )}
 
-          <h2 className="shiny-text" onClick={() => handleClick("Cards")}>Cards</h2>
-          {openList === "Cards" && (
-            <ul>
-              <li onClick={() => setSelectedComponent("Card1")}>Card1</li>
-              <li onClick={() => setSelectedComponent("Card2")}>Card2</li>
-              <li onClick={() => setSelectedComponent("Gojo")}>Gojo</li>
-            </ul>
-          )}
+          {/* Background Section */}
+          <li>
+            <h2 className="shiny-text" onClick={() => handleClick("Background")}>
+              Background
+            </h2>
+            {openList === "Background" && (
+              <ul>
+                <li><Link to="/components/background1">Background1</Link></li>
+                <li><Link to="/components/background2">Background2</Link></li>
+                <li><Link to="/components/background3">Background3</Link></li>
+              </ul>
+            )}
+          </li>
 
-          <h2 className="shiny-text" onClick={() => handleClick("Forms")}>Forms</h2>
-          {openList === "Forms" && (
-            <ul>
-              <li onClick={() => setSelectedComponent("Form1")}>Form1</li>
-              <li onClick={() => setSelectedComponent("Form2")}>Form2</li>
-            </ul>
-          )}
+          {/* Cards Section */}
+          <li>
+            <h2 className="shiny-text" onClick={() => handleClick("Cards")}>
+              Cards
+            </h2>
+            {openList === "Cards" && (
+              <ul>
+                <li><Link to="/components/card1">Card1</Link></li>
+                <li><Link to="/components/card2">Card2</Link></li>
+                <li><Link to="/components/gojo">Gojo</Link></li>
+              </ul>
+            )}
+          </li>
+
+          {/* Forms Section */}
+          <li>
+            <h2 className="shiny-text" onClick={() => handleClick("Forms")}>
+              Forms
+            </h2>
+            {openList === "Forms" && (
+              <ul>
+                <li><Link to="/components/form1">Form1</Link></li>
+                <li><Link to="/components/form2">Form2</Link></li>
+              </ul>
+            )}
+          </li>
+
         </ul>
       </aside>
 
+      {/* Main Display */}
       <main className="main">
-        <MainSection title="Main Display">
-          <h1 className="shiny-text">Main Preview</h1>
-          <BackgroundMain />
-          <CardMain />
-          <FormMain />
+        <MainSection title="Main Preview">
+          <Outlet />
         </MainSection>
       </main>
     </div>
